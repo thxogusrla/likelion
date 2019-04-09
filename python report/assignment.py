6c1f2f57dd92b1bdf7f183b59d92bce288152db9
kimtaehyeon <thxogusrla@gmail.com>
2019-04-09 21:33:57 +0900

    python

---
+++ python report/assignment.py
@@ -0,0 +1,22 @@
+class Profile:
+    array= []
+    def setdata (self, name, phone, gender):
+        self.name = name
+        self.phone = phone
+        self.gender = gender
+        Profile.array =[self.name, self.phone, self.gender]
+a = Profile()
+b = []
+
+while True:
+    name = input("이름을 입력하세요:")
+    if name == "그만": break
+    phone = input("전화번호를 입력하세요:")
+    gender = input("성별을 입력하세요(male 이나 female로 작성해주세요:")
+    if(gender != "male" and gender != "female"): gender ="모르겠네용~"
+    a.setdata(name, phone, gender)
+    b.append(a.array)
+
+for i in range(len(b)):
+    print("이름은 %s, 전화번호는 %s, 성별은 %s입니다." % (b[i][0],b[i][1],b[i][2]))
+
