e0ab61010998132abe48f3bdb08771a2ee023833
kimtaehyeon <thxogusrla@gmail.com>
2019-05-09 21:12:04 +0900

    5/9

---
+++ CRUD/blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('', MainpageView.as_view(), name='mainpage'),
+]
---
+++ CRUD/blog/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
+from django.views.generic. base import TemplateView
+from .forms import BlogForm
+from .models import Blog
+# Create your views here.
+def layout(request):
+    return render(request, 'blog/layout.html')
+
+def home(request):
+    blogs = Blog.objects
+    return render(request, 'blog/home.html', {'blogs': blogs})
+
+def new(request):
+    return render(request, 'blog/new.html')
+
+def create(request):
+    blog = Blog()
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save()
+    return redirect('/blog/home/')
+
+def blogform(request, blog=None):
+    if request.method =='POST':
+        form = BlogForm(request.POST, instance=blog)
+        if form.is_valid():
+            blog = form.save(commit=False)
+            blog.pub_date=timezone.now()
+            blog.save()
+            return redirect('home')
+    else:
+        form = BlogForm(instance=blog)
+        return render(request, 'blog/new.html', {'form':form})
+
+def edit(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    return blogform(request, blog)
+
+def remove(request, pk):
+    blog =get_object_or_404(Blog, pk=pk)
+    blog.delete()
+    return redirect('home')
+
+class MainpageView(TemplateView):
+    template_name = 'blog/main.html'
