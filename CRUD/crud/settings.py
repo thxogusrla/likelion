d8ef03bbab2e3efb11b7ae5c550aaf3975a47d75
kimtaehyeon <thxogusrla@gmail.com>
2019-05-10 16:28:31 +0900

    수정

--- CRUD/crud/settings.py
+++ CRUD/crud/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR, 'blog', 'static')
     ]
 
 STATIC_URL = '/static/'
